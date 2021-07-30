b1bbb627ccc12032fa15a3a47b0332b0286b686c
philz-stizles <theophilusighalo@gmail.com>
2021-07-30 06:22:29 +0100

    fg

--- src/index.js
+++ src/index.js
@@ -2,13 +2,37 @@ require('dotenv').config()
 const express = require('express')
 const chalk = require('chalk')
 require('express-async-errors')
+const cors = require('cors')
 // const { errorHandler } = require('./middlewares/error-handler')
 const NewsAPI = require('newsapi')
 const newsapi = new NewsAPI(process.env.NEWS_API_KEY)
 
 const app = express()
 
-app.set('trust proxy', true)
+// app.set('trust proxy', true)
+
+// Cors
+// app.use((req: Request, res: Response, next): Response | void => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+//   );
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+//   // A fix for graphql response with status of 405 (Method Not Allowed)
+//   if (req.method === 'OPTIONS') {
+//     return res.sendStatus(200);
+//   }
+//   return next();
+// });
+
+// CORS
+app.use(
+  cors()
+  // origin: 'https://someurl.com'
+) //
 
 app.use(express.json())
 
@@ -23,17 +47,23 @@ const getNewsFeed = function (country, category) {
         console.log(response)
         resolve(response)
       })
+    // .catch((err) => console.log(response.message))
   })
 }
 
 app.get('/api/news-feed', async (req, res) => {
-  const { country, category } = req.query
+  try {
+    const { country, category } = req.query
 
-  // To query /v2/top-headlines
-  // All options passed to topHeadlines are optional, but you need to include at least one of them
-  const newsFeed = await getNewsFeed(country, category)
+    // To query /v2/top-headlines
+    // All options passed to topHeadlines are optional, but you need to include at least one of them
+    const newsFeed = await getNewsFeed(country, category)
 
-  return res.send(newsFeed)
+    return res.send(newsFeed)
+  } catch (error) {
+    console.log(error.message)
+    return res.status(500)
+  }
 })
 
 // app.all('*', async (req, res) => {
